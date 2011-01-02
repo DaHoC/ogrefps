d5d282c60b396c39587b6300968f3ba67afd3f3e
Jan Hendriks <dahoc3150@googlemail.com>
2011-01-02 21:07:59 +0100

    -preparation for key configuration via config file parsing

--- src/keyBindings.h
+++ src/keyBindings.h
@@ -27,7 +27,28 @@ public:
 
     }
 
-    std::string getActionForKey(const OIS::KeyCode &keyCode) {
+    /// @TODO: Implement me correctly, see http://www.ogre3d.org/forums/viewtopic.php?f=2&t=61086&p=408221&hilit=keys+map#p408221
+    void parseKeyConfiguration(const std::string& keyConfigFile) {
+        Ogre::ConfigFile cf;
+        cf.load(keyConfigFile);
+        Ogre::ConfigFile::SectionIterator secIt = cf.getSectionIterator();
+        secIt.getNext();
+        Ogre::String sectionstring = "";
+        Ogre::String typestring = "";
+        Ogre::String paramstring = "";
+        while (secIt.hasMoreElements()) {
+            sectionstring = secIt.peekNextKey();
+            Ogre::ConfigFile::SettingsMultiMap* settings = secIt.getNext();
+            //Do the code for section... e.g. print sectionstring
+            for (Ogre::ConfigFile::SettingsMultiMap::iterator setIt = settings->begin(); setIt != settings->end(); ++setIt) {
+                typestring = setIt->first;
+                paramstring = setIt->second;
+                // do the code for key/value
+            }
+        }
+    }
+
+    std::string getActionForKey(const OIS::KeyCode& keyCode) {
         return this->keyMapping.at(keyCode);
     }
 
